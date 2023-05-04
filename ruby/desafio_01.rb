a566c6e5049a8ff423d1e0f5dac3361bc2435e82
GeorgePires <georgeasp.pi@gmail.com>
2023-05-03 21:37:05 -0300

    :sparkles: feat: desafio

--- ruby/desafio_01.rb
+++ ruby/desafio_01.rb
@@ -3,9 +3,9 @@
 # Criar um programa que dê a saída do nome completo do usuário e a sua idade
 
 print 'Digite seu nome:  '
-nome = gets.chomp
+first_name = gets.chomp
 print 'Digite seu sobrenome: '
-sobrenome = gets.chomp
+last_name = gets.chomp
 print 'Digite sua idade: '
-idade = gets.chomp
-puts "Olá #{nome} #{sobrenome}! Seja bem vindo! Sua idade é #{idade}."
+age = gets.chomp
+puts "Olá #{first_name} #{last_name}! Seja bem vindo! Sua idade é #{age}."
