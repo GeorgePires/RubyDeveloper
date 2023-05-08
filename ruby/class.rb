fcd0a66ae73e153681d4208c119a617c26f2373a
GeorgePires <georgeasp.pi@gmail.com>
2023-05-07 22:27:44 -0300

    :sparkles: feat: POO

---
+++ ruby/class.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# definindo classe:
+# A classe é a base para um objeto
+# Exemplo: Eu quero construir um prédio (objeto), para isso eu preciso de uma planta (classe)
+# Resumindo, a classe é uma descrição de como será esse objeto
+# sozinha ela é apenas um desenho, mas junto com o objeto começamos a ver algo prático.
+# veremos as propriedades desse objeto (métodos), tamanho, qts quartos, qts banheiros
